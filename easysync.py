d112a6d484cae23fc1c38ae6c6cbefd7130cf028
Mathew Oakes <mathewo@anglicare-tas.org.au>
2016-05-13 22:26:43 +1000

    Failed attempt at python toga webview and flask

--- easysync.py
+++ easysync.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 from __future__ import print_function, unicode_literals, absolute_import
-
-
+import time
 import toga
+from multiprocessing import Process
+from guts import runner
+
 
 class Swim(toga.App):
 
@@ -15,9 +17,19 @@ class Swim(toga.App):
         container.constrain(self.webview.RIGHT == container.RIGHT)
         container.constrain(self.webview.LEFT == container.LEFT)
         self.main_window.content = container
-        self.webview.url = 'http://google.com.au'
+        self.webview.url = 'http://127.0.0.1:5000/'
+
+    def load_page(self, widget=None):
+        self.webview.url = 'http://127.0.0.1:5000/'
 
 
 if __name__=='__main__':
     app = Swim('SyncSwim', 'org.anglicaretas.syncswim')
+    pp = Process(target=runner)
+    pp.start()
+    time.sleep(5)
     app.main_loop()
+    pp.terminate()
+    pp.join()
+
+
